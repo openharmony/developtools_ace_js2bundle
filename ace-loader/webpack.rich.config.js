f234fa7587d5279f8d8a4cb46706fd301226de3a
lizhouze <lizhouze@huawei.com>
2022-04-13 15:20:21 +0800

    fixed 9356295 from https://gitee.com/kage1/developtools_ace-js2bundle/pulls/224 lizhouze@huawei.com
    Signed-off-by: lizhouze <lizhouze@huawei.com>

--- ace-loader/webpack.rich.config.js
+++ ace-loader/webpack.rich.config.js
@@ -281,27 +281,29 @@ module.exports = (env) => {
       notPreview(env)
     }
   } else {
-    config.optimization = {
-      splitChunks: {
-        chunks(chunk) {
-          return !/^\.\/workers\//.test(chunk.name) && !/^\.\/TestAbility/.test(chunk.name);
-        },
-        minSize: 0,
-        cacheGroups: {
-          vendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            name: "vendors",
+    if (process.env.abilityType === 'page') {
+      config.optimization = {
+        splitChunks: {
+          chunks(chunk) {
+            return !/^\.\/workers\//.test(chunk.name) && !/^\.\/TestAbility/.test(chunk.name);
           },
-          commons: {
-            test: /\.js|css|hml$/,
-            name: 'commons',
-            priority: -20,
-            minChunks: 2,
-            reuseExistingChunk: true
+          minSize: 0,
+          cacheGroups: {
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              name: "vendors",
+            },
+            commons: {
+              test: /\.js|css|hml$/,
+              name: 'commons',
+              priority: -20,
+              minChunks: 2,
+              reuseExistingChunk: true
+            }
           }
-        }
-      },
+        },
+      }
     }
     if (env.isPreview !== "true") {
       notPreview(env)

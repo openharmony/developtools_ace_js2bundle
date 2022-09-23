8db2f5f5313e3156294581e20e6c30e42d1f2050
zhangrengao <zhangrengao1@huawei.com>
2022-09-23 10:27:09 +0800

    Pick SDK ark code change
    Signed-off-by: zhangrengao <zhangrengao1@huawei.com>
    Change-Id: I719c0eee7007353f6ad74efe9093ec717e3e02c0

--- ace-loader/src/gen-abc.js
+++ ace-loader/src/gen-abc.js
@@ -20,32 +20,53 @@ const SUCCESS = 0;
 const FAIL = 1;
 const red = '\u001b[31m';
 const reset = '\u001b[39m';
+const TS2ABC = 'ts2abc';
+const ES2ABC = 'es2abc';
 
 function js2abcByWorkers(jsonInput, cmd) {
   const inputPaths = JSON.parse(jsonInput);
   for (let i = 0; i < inputPaths.length; ++i) {
-    let input = inputPaths[i].path;
-    let singleCmd = `${cmd} "${input}"`;
+    // for matching debug info mechanism
+    const input = inputPaths[i].path.replace(/\.temp\.js$/, "_.js");
+    const cacheOutputPath = inputPaths[i].cacheOutputPath;
+    const cacheAbcFilePath = cacheOutputPath.replace(/\.temp\.js$/, ".abc");
+    const singleCmd = `${cmd} "${cacheOutputPath}" -o "${cacheAbcFilePath}" --source-file "${input}"`;
     try {
       childProcess.execSync(singleCmd);
     } catch (e) {
-      console.error(red, `ETS:ERROR Failed to convert file ${input} to abc `, reset);
-      process.exit(FAIL);
-    }
-
-    const abcFile = input.replace(/\.js$/, '.abc');
-    if (fs.existsSync(abcFile)) {
-      const abcFileNew = abcFile.replace(/_.abc$/, '.abc');
-      fs.copyFileSync(abcFile, abcFileNew);
-      fs.unlinkSync(abcFile);
-    } else {
-      console.error(red, `ETS:ERROR ${abcFile} is lost`, reset);
+      console.debug(red, `ERROR Failed to convert file ${input} to abc `, reset);
       process.exit(FAIL);
     }
   }
 }
 
+function es2abcByWorkers(jsonInput, cmd) {
+  const inputPaths = JSON.parse(jsonInput);
+  for (let i = 0; i < inputPaths.length; ++i) {
+    const input = inputPaths[i].path.replace(/\.temp\.js$/, "_.js");
+    const cacheOutputPath = inputPaths[i].cacheOutputPath;
+    const cacheAbcFilePath = cacheOutputPath.replace(/\.temp\.js$/, ".abc");
+    const singleCmd = `${cmd} "${cacheOutputPath}" --output "${cacheAbcFilePath}" --source-file "${input}"`;
+    console.debug('gen abc cmd is: ', singleCmd, ' ,file size is:', inputPaths[i].size, ' byte');
+    try {
+      childProcess.execSync(singleCmd);
+    } catch (e) {
+      console.error(red, `ERROR Failed to convert file ${input} to abc `, reset);
+      process.exit(FAIL);
+    }
+  }
+
+  return;
+}
+
 if (cluster.isWorker && process.env["inputs"] !== undefined && process.env["cmd"] !== undefined) {
-  js2abcByWorkers(process.env["inputs"], process.env["cmd"]);
+  if (process.env.panda === TS2ABC) {
+    js2abcByWorkers(process.env["inputs"], process.env["cmd"]);
+  } else if (process.env.panda === ES2ABC  || process.env.panda === 'undefined' || process.env.panda === undefined) {
+    es2abcByWorkers(process.env["inputs"], process.env["cmd"]);
+  } else {
+    logger.error(red, `ERROR please set panda module`, reset);
+    process.exit(FAIL);
+  }
   process.exit(SUCCESS);
 }

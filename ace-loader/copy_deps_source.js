80456b953b262d369611404b86dbf39cf82e2f02
kage1 <lizhouze@huawei.com>
2026-02-28 14:22:13 +0800

    lizhouze@huawei.com
    Signed-off-by: kage1 <lizhouze@huawei.com>

--- ace-loader/copy_deps_source.js
+++ ace-loader/copy_deps_source.js
@@ -17,6 +17,15 @@ const fs = require('fs');
 const path = require('path');
 const moduleSource = require('./module-source');
 
+/**
+ * Ensure destination exists before copying.
+ * - Skip if source path ends with /test
+ * - If destination exists, invoke callback immediately
+ * - If destination does not exist, create it and then invoke callback
+ * @param {string} src - Source path
+ * @param {string} dst - Destination path
+ * @param {Function} callback - Callback receiving (src, dst)
+ */
 const exists = function(src, dst, callback) {
   if (src.match(/\/test$/)) {
     return;
@@ -33,6 +42,12 @@ const exists = function(src, dst, callback) {
 };
 
 stat = fs.stat;
+/**
+ * Recursively copy contents from src directory to dst directory.
+ * Iterates over items in src and delegates handling to copyForEach.
+ * @param {string} src - Source directory
+ * @param {string} dst - Destination directory
+ */
 const copy = function(src, dst) {
   fs.readdir(src, function(err, paths) {
       if (err) {
@@ -44,6 +59,14 @@ const copy = function(src, dst) {
   });
 };
 
+/**
+ * Copy a single item from src to dst, handling files and subdirectories.
+ * - If it's a file, copy only non-gulpfile and .js extensions, otherwise skip
+ * - If it's a directory, ensure destination exists and recurse
+ * @param {string} src - Source directory
+ * @param {string} dst - Destination directory
+ * @param {string} _path - Path name during traversal (relative)
+ */
 function copyForEach(src, dst, _path) {
   let _src = src + '/' + _path;
   let _dst = dst + '/' + _path;
@@ -67,6 +90,10 @@ function copyForEach(src, dst, _path) {
   });
 }
 
+/**
+ * Copy external dependency resources to the target output directory.
+ * The output root directory is specified by the command line argument process.argv[2].
+ */
 moduleSource.copyResource(path.resolve(__dirname, './third_party/weex-loader/deps/weex-scripter'), process.argv[2] + '/scripter');
 moduleSource.copyResource(path.resolve(__dirname, './third_party/weex-loader/deps/weex-styler'), process.argv[2] + '/styler');
 moduleSource.copyResource(path.resolve(__dirname, './third_party/parse5/packages/parse5/dist/cjs'), process.argv[2] + '/parse');
